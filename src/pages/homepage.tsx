63b4947bac488f91a6a7dc16cba727809c35a8cc
Lê Nguyễn Phương Thái <114068860+henrylenguyen@users.noreply.github.com>
2024-08-02 20:08:25 +0700

    build project

--- src/pages/homepage.tsx
+++ src/pages/homepage.tsx
@@ -6,7 +6,7 @@ interface IHomePageProps {}
 const HomePage: React.FunctionComponent<IHomePageProps> = (props) => {
   return (
     <div className='bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3'>
-      <Button variant='outline'>Button</Button>
+      hehe
     </div>
   )
 }
